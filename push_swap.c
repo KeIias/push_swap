3d099eba957f364a0e3e6aa98fee9fe57c6faa53
Aleksi GAUTIER <algautie@e1r6p21.42.fr>
2019-08-13 03:06:43 +0200

    optimized some stuff

--- push_swap.c
+++ push_swap.c
@@ -25,28 +25,6 @@ int		get_mid(t_dlist *beg)
 	return (ret);
 }
 
-void	separate_in_two(t_dlist **beg_a, t_dlist **beg_b, t_dlist **beg_ins)
-{
-	int		mid;
-	int		len;
-
-	mid = get_mid(*beg_a);
-	len = ft_lstlen(*beg_a);
-	if ((*beg_a)->value < mid)
-		push(beg_a, beg_b, beg_ins, P_B);
-	rotate(beg_a, beg_ins, R_A);
-	while (ft_lstlen(*beg_b) != len / 2)
-	{
-		//ft_lst_print(*beg_a);
-		//ft_lst_print(*beg_b);
-		//ft_lst_print(*beg_ins);
-		//ft_putchar('\n');
-		if ((*beg_a)->value < mid)
-			push(beg_a, beg_b, beg_ins, P_B);
-		rotate(beg_a, beg_ins, R_A);
-	}
-}
-
 int		get_first_one(t_dlist **beg_a, int mask)
 {
 	t_dlist *tmp;
@@ -61,24 +39,68 @@ int		get_first_one(t_dlist **beg_a, int mask)
 	return (0);
 }
 
+int		are_zeros_left(t_dlist **beg_a, int mask)
+{
+	t_dlist *tmp;
+
+	tmp = *beg_a;
+	while (tmp)
+	{
+		if (!(tmp->value & mask))
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+void	back_to_first(t_dlist **beg_a, t_dlist **beg_ins, int first_one)
+{
+	t_dlist	*tmp;
+	int		pos;
+
+	tmp = *beg_a;
+	pos = 0;
+	while (tmp->value != first_one && ++pos)
+		tmp = tmp->next;
+	while ((*beg_a)->value != first_one)
+	{
+		if (pos * 2 > ft_lstlen(*beg_a))
+			reverse_rotate(beg_a, beg_ins, RR_A);
+		else
+			rotate(beg_a, beg_ins, R_A);
+	}
+}
+
 void	radix(t_dlist **beg_a, t_dlist **beg_b, t_dlist **beg_ins)
 {
 	int mask;
 	int first_one;
 
 	mask = 1;
-	while (!is_sorted(*beg_a) || ft_lstlen(*beg_b) != 0)
+	while (!is_sorted(*beg_a))
 	{
 		if ((first_one = get_first_one(beg_a, mask)) != 0)
 		{
-
+			while (are_zeros_left(beg_a, mask))
+			{
+				if (is_sorted(*beg_a) && ft_lstlen(*beg_b) == 0)
+					break;
+				if (!((*beg_a)->value & mask))
+					push(beg_a, beg_b, beg_ins, P_B);
+				if ((*beg_a)->value & mask && are_zeros_left(beg_a, mask))
+					rotate(beg_a, beg_ins, R_A);
+			}
+			if (!(is_sorted(*beg_a) && ft_lstlen(*beg_b) == 0))
+				back_to_first(beg_a, beg_ins, first_one);
+			while (ft_lstlen(*beg_b) != 0)
+				push(beg_b, beg_a, beg_ins, P_A);
 		}
 		mask *= 2;
 	}
 }
 
 int		main(int ac, char **av)
-{
+{ 
 	int		i;
 	t_dlist	*beg_a;
 	t_dlist	*beg_b;
@@ -95,11 +117,6 @@ int		main(int ac, char **av)
 	i = 1;
 	while (++i < ac)
 		ft_lst_push_back(&beg_a, ft_atoi(av[i]));
-	separate_in_two(&beg_a, &beg_b, &beg_ins);
-	//ft_lst_print(beg_a);
-	//ft_putchar('\n');
-	//ft_lst_print(beg_b);
-	//ft_putchar('\n');
-	//ft_lst_print(beg_ins);
+	radix(&beg_a, &beg_b, &beg_ins);
 	print_ins(beg_ins);
 }
