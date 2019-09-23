0490e3168c496972c65a0c9b143043abc6a00782
MBA-Aleksi <aleksi.gautier@gmail.com>
2019-09-23 22:49:43 +0200

    added pseudo quick_sort

--- srcs/push_swap/pseudo_quick_sort.c
+++ srcs/push_swap/pseudo_quick_sort.c
@@ -19,32 +19,17 @@ void	sort_three(t_dlist **beg, t_dlist **beg_ins)
 	int b;
 	int c;
 
-	tmp = *beg;
 	while (!(is_sorted(*beg)))
 	{
+		tmp = *beg;
 		a = tmp->value;
 		b = tmp->next->value;
 		c = tmp->next->next->value;
-		ft_putnbr(a);
-		ft_putchar('\n');
-		ft_putnbr(b);
-		ft_putchar('\n');
-		ft_putnbr(c);
-		ft_putchar('\n');
-		ft_putchar('\n');
 		if (a < b && a < c && b > c)
-		{
-			ft_putstr("Wut");
 			ps_reverse_rotate(beg, beg_ins, RR_A);
-			ft_putstr("Face");
-		}
 		else if (a > b && b < c && c > a)
-		{
-			ft_putstr("Pog");
 			ps_swap(beg, beg_ins, S_A);
-			ft_putstr("Champ");
-		}
-		else if (a > b && a > c && b > c)
+		else if (a < b && a > c && b > c)
 			ps_reverse_rotate(beg, beg_ins, RR_A);
 		else if (a > b && a > c && b > c)
 			ps_swap(beg, beg_ins, S_A);
@@ -53,8 +38,46 @@ void	sort_three(t_dlist **beg, t_dlist **beg_ins)
 	}
 }
 
+int		get_lowest(t_dlist **beg)
+{
+	int		value;
+	t_dlist	*tmp;
+
+	tmp = *beg;
+	value = tmp->value;
+	while (tmp)
+	{
+		if (tmp->value < value)
+			value = tmp->value;
+		tmp = tmp->next;
+	}
+	return (value);
+}
+
 void	pseudo_quick_sort(t_dlist **beg_a, t_dlist **beg_b, t_dlist **beg_ins)
 {
-	(void)beg_b;
+	int		lowest;
+	int		pos;
+	t_dlist	*tmp;
+
+	if (is_sorted(*beg_a))
+		return ;
+	while (ft_lstlen(*beg_a) != 3)
+	{
+		lowest = get_lowest(beg_a);
+		pos = 0;
+		tmp = *beg_a;
+		while (tmp->value != lowest && ++pos)
+			tmp = tmp->next;
+		while ((*beg_a)->value != lowest)
+		{
+			if (pos * 2 > ft_lstlen(*beg_a))
+				ps_reverse_rotate(beg_a, beg_ins, RR_A);
+			else
+				ps_rotate(beg_a, beg_ins, R_A);
+	}
+		ps_push(beg_a, beg_b, beg_ins, P_B);
+	}
 	sort_three(beg_a, beg_ins);
+	push_all(beg_b, beg_a, beg_ins, P_A);
 }
